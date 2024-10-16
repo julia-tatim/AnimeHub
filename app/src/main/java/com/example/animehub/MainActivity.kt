43023c4007cfddba4735c27ff4fcb9e713bb88de
ImnotSoul <carlos.rosemberg24@gmail.com>
2024-10-16 16:00:06 -0300

    testebd

--- app/src/main/java/com/example/animehub/MainActivity.kt
+++ app/src/main/java/com/example/animehub/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.animehub
 
+
+import com.example.animehub.Cadastroact
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.databinding.DataBindingUtil
 import com.example.animehub.databinding.ActivityMainBinding
 
+
 class MainActivity : ComponentActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,7 +24,10 @@ class MainActivity : ComponentActivity() {
         }
 
         binding.signUpButton.setOnClickListener {
-            val intent = Intent(this, cadastroact::class.java)
+            val intent = Intent(
+                this,
+                Cadastroact::class.java
+            )
             startActivity(intent)
         }
     }
